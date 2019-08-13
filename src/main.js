52069b62ee7ed46879858018ffe5869d98f8d726
cjlozano <carlosjlp@gmail.com>
2019-08-13 21:10:03 +0200

    añadida configuración graphql

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue';
+import VueApollo from 'vue-apollo';
 import App from './App.vue';
+import apolloCliente from './graphql/clienteApollo';
 
+Vue.use(VueApollo);
 Vue.config.productionTip = false;
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloCliente,
+});
 
 new Vue({
   render: h => h(App),
+  apolloProvider,
 }).$mount('#app');
