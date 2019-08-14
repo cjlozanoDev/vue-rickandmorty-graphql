fbde29db6273f5ab81ac904de6d686dbc76043c0
cjlozano <carlosjlp@gmail.com>
2019-08-14 13:36:05 +0200

    creado el componente de herramientas de búsqueda de personajes

---
+++ src/components/HerramientasBusqueda.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="herramientas">
+      <div>
+        <v-select
+          v-model="estadoPersonaje"
+          :items="items"
+          solo
+        ></v-select>
+      </div>
+      <div class="text-field">
+        <v-text-field
+          placeholder="Nombre del personaje"
+          solo
+        ></v-text-field>
+      </div>
+      <div class="boton">
+        <v-btn
+          color="#08B2CA"
+          class="white--text"
+        >Buscar</v-btn>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HerramientasBusqueda',
+  data() {
+    return {
+      texto: 'herramientas',
+      items: [
+        {
+          text: 'Vivo',
+          value: 'alive',
+        },
+        {
+          text: 'Muerto',
+          value: 'dead',
+        },
+        {
+          text: 'Desconocido',
+          value: 'unknown',
+        },
+      ],
+      estadoPersonaje: 'alive',
+    };
+  },
+};
+</script>
+
+<style scoped>
+.herramientas {
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+}
+.text-field, .boton {
+  margin-left: 10px;
+}
+.boton {
+  color: white;
+}
+</style>
