e46464d4d04155b6b859c57a52469fe3c9de5955
cjlozano <carlosjlp@gmail.com>
2019-08-14 12:40:42 +0200

    añadida cabecera rick and morty

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <v-app>
-    <v-content transition="slide-x-transition">
-      <main class="app">
-        <header class="header">
-          <h1> Rick and Morty</h1>
-        </header>
+    <main class="app">
+      <v-content transition="slide-x-transition">
+        <v-toolbar color ="#1B1847" img="">
+          <div class="contenedor-imagen">
+            <v-img
+              src="./assets/rick_and_morty.png"
+              max-height="80px"
+              contain
+            />
+          </div>
+        </v-toolbar>
         <section class="section">
           <router-view></router-view>
         </section>
-      </main>
-    </v-content>
+      </v-content>
+    </main>
   </v-app>
 </template>
 <script>
@@ -19,4 +25,15 @@ export default {
 </script>
 
 <style scoped>
+.app {
+  min-height: 100%;
+  background-color: #F4F4F4;
+}
+.section {
+  padding: 20px
+}
+.contenedor-imagen {
+  width: 100%;
+  height: 80px;
+}
 </style>
