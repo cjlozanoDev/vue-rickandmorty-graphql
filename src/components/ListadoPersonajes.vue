2291d50dd54523ad2b87a9ce0dd4224cea7c4cc1
cjlozano <carlosjlp@gmail.com>
2019-09-05 12:21:04 +0200

    añadida paginación de las tarjetas de los personajes

--- src/components/ListadoPersonajes.vue
+++ src/components/ListadoPersonajes.vue
@@ -1,17 +1,25 @@
 <template>
   <v-container
      fluid>
-    <v-layout wrap>
+    <v-layout
+      wrap
+      :justify-center="esCentrado">
       <v-flex
           xs6
           sm4
           md3
           pa-2
-          v-for="(personaje, id) in listadoPersonajes.results"
+          v-for="(personaje, id) in listadoFiltrado"
           :key="id">
         <TarjetaPersonaje
           :personaje="personaje"/>
       </v-flex>
+      <div class="bloque-numeros-paginas">
+        <v-pagination
+            v-model="paginaActual"
+            :length="numeroPaginasTotales">
+        </v-pagination>
+      </div>
     </v-layout>
   </v-container>
 </template>
@@ -25,21 +33,52 @@ export default {
     TarjetaPersonaje,
   },
   data() {
-    return {};
+    return {
+      numeroTarjetasPorPagina: 8,
+      paginaActual: 1,
+    };
   },
   computed: {
     ...mapGetters({
       listadoPersonajes: 'listadoPersonajes',
     }),
+    numeroPaginasTotales() {
+      if (this.listadoPersonajes.length === 0) {
+        return 0;
+      }
+      return Math.ceil(this.listadoPersonajes.results.length / this.numeroTarjetasPorPagina);
+    },
+    listadoFiltrado() {
+      if (this.listadoPersonajes.length === 0) {
+        return this.listadoPersonajes;
+      }
+      const indiceComienzo = (this.paginaActual - 1) * this.numeroTarjetasPorPagina;
+      const arrayFiltrado = this.listadoPersonajes.results.slice(indiceComienzo, indiceComienzo
+        + this.numeroTarjetasPorPagina);
+      return arrayFiltrado;
+    },
+    esCentrado() {
+      const width = (window.innerWidth > 0) ? window.innerWidth : window.screen.width;
+      if (this.listadoFiltrado.length < 4 && width > 600) {
+        return true;
+      }
+      return false;
+    },
   },
   methods: {
+    irANumeroPagina(numero) {
+      this.paginaActual = numero;
+    },
   },
 };
 </script>
 <style scoped>
-@media only screen and (min-width: 960px) {
-.container {
-  max-width: 900px;
+.bloque-numeros-paginas {
+  width: 100%;
 }
+@media only screen and (min-width: 960px) {
+  .container {
+    max-width: 900px;
+  }
 }
 </style>
